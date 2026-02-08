e8ebdd543d133f98e082a86ed45ca0b2896ec664
Ayush Thakur <ayusht1712@gmail.com>
2026-02-08 20:54:13 +0530

    Completed python logic assignment 1

---
+++ python-logic-assignment 1/access_control.py
@@ -0,0 +1,7 @@
+age = int(input("Enter age: "))
+has_id_input = input("Do you have an ID (True/False): ")
+
+has_id = has_id_input.lower() == "true"
+
+if age >= 18 and has_id:
+    print("Entry allowed")
